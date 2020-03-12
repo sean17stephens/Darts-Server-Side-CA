8c3c49df3e2f225b084d091fa021d2aeb2d59627
seanstephens17 <57396679+seanstephens17@users.noreply.github.com>
2020-03-12 00:47:44 +0000

    record updated to player

--- delete_player.php
+++ delete_player.php
@@ -3,7 +3,7 @@ require_once('database.php');
 // Get IDs
 $player_id = filter_input(INPUT_POST, 'player_id', FILTER_VALIDATE_INT);
 $category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
-// Delete the record from the database
+// Delete the player from the database
 if ($player_id != false && $category_id != false) {
     $query = "DELETE FROM players
               WHERE recordID = :player_id";
