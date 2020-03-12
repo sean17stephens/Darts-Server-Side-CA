5c221a97ce6ae8d7a20cb094df7d9f2bd31a9383
Sean Stephens <D00211442@student.dkit.ie>
2020-03-12 11:52:10 +0000

    Adding Players, working. Now not working

--- add_category.php
+++ add_category.php
@@ -1,17 +1,17 @@
 <?php
 // Get the category data
-$dart = $dart = filter_input(INPUT_POST, 'dart');
+$name = $name = filter_input(INPUT_POST, 'name');
 // Validate inputs
-if ($dart == null) {
+if ($name == null) {
     $error = "Invalid category data. Check all fields and try again.";
     include('error.php');
 } else {
     require_once('database.php');
     // Add the record to the database
     $query = "INSERT INTO categories (categoryName)
-              VALUES (:dart)";
+              VALUES (:name)";
     $statement = $db->prepare($query);
-    $statement->bindValue(':dart', $dart);
+    $statement->bindValue(':name', $name);
     $statement->execute();
     $statement->closeCursor();
     // Display the Category List page
